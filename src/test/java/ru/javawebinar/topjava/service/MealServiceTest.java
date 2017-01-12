ee6cfd1083a0fb75e40d0c1a4a712670e550cb94
Maksim Tkachenko <osbornroad@gmail.com>
2017-01-12 10:09:20 +0300

    Optional done

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,7 +1,14 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.ExternalResource;
+import org.junit.rules.Stopwatch;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.junit.runners.model.Statement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
@@ -10,9 +17,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
@@ -29,6 +42,26 @@ public class MealServiceTest {
     @Autowired
     private MealService service;
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private static final Logger logger = Logger.getLogger("");
+
+    private static void logInfo(Description description, String status, long nanos) {
+        String testName = description.getMethodName();
+        logger.info(String.format("Test %s %s, spent %d microseconds",
+                testName, status, TimeUnit.NANOSECONDS.toMicros(nanos)));
+    }
+
+    @Rule
+    public Stopwatch stopwatch = new Stopwatch() {
+        @Override
+        protected void finished(long nanos, Description description) {
+            logInfo(description, "finished", nanos);
+            System.out.println(String.format("Test %s finished, spent %d microseconds", description.getMethodName(), TimeUnit.NANOSECONDS.toMicros(nanos)));
+        }
+    };
+
     //Ok
     @Test
     public void testDelete() throws Exception {
@@ -37,8 +70,10 @@ public class MealServiceTest {
     }
 
     //Ok
-    @Test(expected = NotFoundException.class)
+    @Test//(expected = NotFoundException.class)
     public void testDeleteNotFound() throws Exception {
+        thrown.expect(NotFoundException.class);
+        thrown.expectMessage("Попытка удалить несуществующую или чужую еду");
         service.delete(MEAL1_ID, 1);
     }
 
@@ -57,9 +92,11 @@ public class MealServiceTest {
         MATCHER.assertEquals(ADMIN_MEAL1, actual);
     }
 
-    //Ok
-    @Test(expected = NotFoundException.class)
+
+    @Test //(expected = NotFoundException.class)
     public void testGetNotFound() throws Exception {
+        thrown.expect(NotFoundException.class);
+        thrown.expectMessage("Попытка получить несуществующую или чужую еду");
         service.get(MEAL1_ID, ADMIN_ID);
     }
 
@@ -72,8 +109,10 @@ public class MealServiceTest {
     }
 
     //Ok
-    @Test(expected = NotFoundException.class)
+    @Test//(expected = NotFoundException.class)
     public void testUpdateNotFound() throws Exception {
+        thrown.expect(NotFoundException.class);
+        thrown.expectMessage("Попытка обновить чужую еду");
         Meal testMeal = service.get(ADMIN_MEAL_ID, ADMIN_ID);
         service.update(testMeal, USER_ID);
     }
