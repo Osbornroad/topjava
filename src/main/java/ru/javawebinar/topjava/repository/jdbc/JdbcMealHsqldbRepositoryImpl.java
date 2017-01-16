3f0c62d5eefae8a00798b3d681f874d3be5a308c
Maksim Tkachenko <osbornroad@gmail.com>
2017-01-16 23:35:58 +0400

    Set done

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealHsqldbRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealHsqldbRepositoryImpl.java
@@ -2,14 +2,18 @@ package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Meal;
 
 import javax.sql.DataSource;
+import java.sql.Timestamp;
+
 
 /**
  * Created by User on 16.01.2017.
  */
+@Repository
 @Profile(Profiles.HSQLDB)
 public class JdbcMealHsqldbRepositoryImpl extends JdbcMealRepositoryImpl {
 
@@ -19,11 +23,14 @@ public class JdbcMealHsqldbRepositoryImpl extends JdbcMealRepositoryImpl {
 
     @Override
     public Meal save(Meal meal, int userId) {
+
+        Timestamp t = Timestamp.valueOf(meal.getDateTime());
+
         MapSqlParameterSource map = new MapSqlParameterSource()
                 .addValue("id", meal.getId())
                 .addValue("description", meal.getDescription())
                 .addValue("calories", meal.getCalories())
-                .addValue("date_time", meal.getDateTime())
+                .addValue("date_time", t)
                 .addValue("user_id", userId);
 
         if (meal.isNew()) {
